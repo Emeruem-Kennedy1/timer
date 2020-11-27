9136357e6c96d2ba06e5d0da4e2cbc276934ef48
Emeruem-Kennedy1 <69385717+Emeruem-Kennedy1@users.noreply.github.com>
2020-11-27 11:15:51 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+
+
+var days = document.getElementById('days');
+var hours = document.getElementById('hours');
+var minutes = document.getElementById('minutes');
+var seconds = document.getElementById('seconds');
+
+const newYear = new Date('15 dec 2020')
+
+function remainingTime() {
+    const time = new Date()
+    const targetDate  = newYear;
+
+    const secs =(targetDate  - time)/1000;
+    const daysLeft = Math.floor(secs/3600/24);
+    const hoursLeft = Math.floor(secs/3600 % 24);
+    const minutesLeft = Math.floor(secs/60 % 60);
+    const secondsLeft = Math.floor(secs % 60);
+
+    days.innerHTML = daysLeft;
+    hours.innerHTML = hoursLeft;
+    minutes.innerHTML = minutesLeft
+    seconds.innerHTML = secondsLeft;
+
+    if (secondsLeft < 10 ) {
+        seconds.innerHTML = '0'+secondsLeft;
+    }
+    if (hoursLeft < 10 ) {
+        hours.innerHTML = '0'+hoursLeft;
+    }
+    if (daysLeft < 10 ) {
+        days.innerHTML = '0'+daysLeft;
+    }
+    if (minutesLeft < 10 ) {
+        minutes.innerHTML = '0'+minutesLeft;
+    }
+};
+
+remainingTime();
+setInterval(remainingTime, 1000);
+
